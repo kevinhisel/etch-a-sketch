46ec2361a8d55d0ea7ce943be82079c3c62bbab5
Kevin Hisel <kevinhisel@gmail.com>
2021-10-20 00:35:26 -0500

    Implement createGrid() to dynamically create grids of various sizes

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+const DEFAULT_SQ = 16;
 const container = document.querySelector('#container');
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.className = 'square16';
-        container.appendChild(square);
 
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'red';
-        });
+createGrid(DEFAULT_SQ);
+
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const square = document.createElement('div');
+            square.className = 'square';
+            square.style.height    = `${ 100 / gridSize }%`;
+            square.style.flexBasis = `${ 100 / gridSize }%`;
+            container.appendChild(square);
+
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'red';
+            });
+        }
     }
 }
