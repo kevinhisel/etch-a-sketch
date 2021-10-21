fa0fbcc82645453b402d65896a19cdc3fe5591fe
Kevin Hisel <kevinhisel@gmail.com>
2021-10-21 10:34:53 -0500

    Remove flex-basis logic for determining sizing of grid

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ createGrid(DEFAULT_SQ);
 
 function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
         for (let j = 0; j < gridSize; j++) {
             const square = document.createElement('div');
             square.className = 'square';
-            square.style.height    = `${ 100 / gridSize }%`;
-            square.style.flexBasis = `${ 100 / gridSize }%`;
-            container.appendChild(square);
+            row.appendChild(square);
 
             square.addEventListener('mouseover', () => {
                 square.style.backgroundColor = 'red';
